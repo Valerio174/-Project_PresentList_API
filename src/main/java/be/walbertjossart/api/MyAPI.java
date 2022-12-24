c96dd1a0b48e608215724678588df9e452c54dd4
nicojoss <nico.jossart@condorcet.be>
2022-12-24 11:07:04 +0100

    pull nico

--- src/main/java/be/walbertjossart/api/MyAPI.java
+++ src/main/java/be/walbertjossart/api/MyAPI.java
@@ -3,6 +3,6 @@ package be.walbertjossart.api;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/*Class that serves as the root of all my API*/
+/*Class that serves as the root of all my API nico*/
 @ApplicationPath("/api")
 public class MyAPI extends Application {}
